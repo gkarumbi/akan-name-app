fc391765b6f8935a443d38c83aade19a4adabbdc
gkarumbi <gkarumbi@gmail.com>
2020-05-04 15:23:01 +0300

    Added js file as a functionality test enviroment

---
+++ js/backup.js
@@ -0,0 +1,20 @@
+
+var userInput = function(){
+    var btnListener  = document.getElementById("submit");
+btnListener.addEventListener("click",function(){
+      
+
+
+
+//Function that outputs the day of the week
+
+
+var yearOfBirth = document.getElementById("yob").value;
+
+var centuryDigits = parseInt(yearOfBirth.substring(-1,2));
+var yearDigits = parseInt(yearOfBirth.substring(2,4));
+var month = parseInt(document.getElementById("month").value);
+var dayOfMonth = parseInt(document.getElementById("date").value);
+var gender;
+
+}
